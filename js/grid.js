a2cd4254ee6dfcaa8e8a10751e68e8e8c5d4a6be
Jungle Tech <williamjung@christians-mbp.home>
2014-08-09 12:31:27 -0400

    ad update

--- js/grid.js
+++ js/grid.js
@@ -2,9 +2,6 @@ function Grid(size, previousState) {
   this.size = size;
   this.cells = previousState ? this.fromState(previousState) : this.empty();
 }
-
-
-
 // Build a grid of the specified size
 Grid.prototype.empty = function () {
   var cells = [];
